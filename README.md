e5be9e06456eee857fc7e5c7df6ce828ab85fcb8
Margus Pärt <margus@wave.ee>
2018-06-17 18:44:35 +0300

    Simple Elixir API hello response is working

---
+++ README.md
@@ -0,0 +1,13 @@
+# Overall
+
+## Client
+
+* Is a simple React app, with custom dispatching, state update and event
+  proxy to API.
+
+## API
+
+* Is a simple Elixir HTTP server with custom "commands".
+
+* Manual used to setup HTTP server
+  https://www.jungledisk.com/blog/2018/03/19/tutorial-a-simple-http-server-in-elixir/
---
+++ api/mix.exs
@@ -0,0 +1,30 @@
+defmodule Api.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :api,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :cowboy, :plug, :poison],
+      mod: {Api.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:cowboy, "~> 1.0.0"},
+      {:plug, "~> 1.5"},
+      {:poison, "~> 3.1"}
+    ]
+  end
+end
