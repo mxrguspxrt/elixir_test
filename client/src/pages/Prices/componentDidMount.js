1474160e4e192525c9bf31273fca4711b3d41576
Margus Pärt <margus@wave.ee>
2018-06-18 04:43:07 +0300

    Added autorefresh

--- client/src/pages/Prices/componentDidMount.js
+++ client/src/pages/Prices/componentDidMount.js
@@ -3,4 +3,8 @@ import {LOAD_PRICES_REQUEST} from '../../constants'
 export default function() {
   const {app} = this.props
   app.dispatch(LOAD_PRICES_REQUEST)
+
+  this.refreshInterval = setInterval(() => {
+    app.dispatch(LOAD_PRICES_REQUEST)
+  }, 1000 * 60 * 5)
 }
--- client/src/pages/Prices/index.js
+++ client/src/pages/Prices/index.js
@@ -1,10 +1,12 @@
 import {Component} from 'react'
 import componentDidMount from './componentDidMount'
+import componentWillUnmount from './componentWillUnmount'
 import render from './render'
 import './index.css'
 
 class Prices extends Component {
   componentDidMount = componentDidMount
+  componentWillUnmount = componentWillUnmount
   render = render
 }
 
